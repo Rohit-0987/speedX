6a6211f11741a2dcfad16cece18c2cfae86c3c98
Rohit-0987 <99091666+Rohit-0987@users.noreply.github.com>
2024-08-08 11:34:06 +0530

    modify app.jsx for InputeForm

--- speedx-frontend/src/App.jsx
+++ speedx-frontend/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import InputForm from './components/InputForm';
+import PerformanceMetrics from './components/PerformanceMetrics';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [metrics, setMetrics] = useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div >
+      <h1 className='my-16 text-center text-2xl text-white font-bold'>SpeedX - Website Performance Analyzer</h1>
+      <InputForm setMetrics={setMetrics} />
+    </div>
+  );
+};
 
-export default App
+export default App;
